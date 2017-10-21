1538e18dd09532f819d3a22b61f2eeb65650e539
Jordi Mundo <jordi.mundo@ZHQ-00383.local>
2017-10-21 14:17:00 +0100

    -Sort list items by date

--- src/MessagesList.jsx
+++ src/MessagesList.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import MessagesListItem from './MessagesListItem';
 
 export default ({users, messages}) => {
+  const sortMessages = () => messages.messages.sort((a,b)=>{
+    const aTime = new Date(a.timestamp);
+    const bTime = new Date(b.timestamp);
+    if(aTime > bTime) return 1;
+    if(aTime < bTime) return -1;
+    return 0;
+  });
+
   const getUserList = ()=>{
     if(messages.isPending)
       return (<li>Loading Messages ...</li>);
+
+    sortMessages();
     return messages.messages.map((i, idx)=>{
       return <MessagesListItem key={idx} message={i} users={users}/>
-    })
+    });
   }
   return (
     <div>
--- src/MessagesListItem.jsx
+++ src/MessagesListItem.jsx
@@ -6,10 +6,10 @@ export default ({users, message}) => {
   return (
     <li>
       <div>
-        {!user ? 'Loading user...' : <img alt={`${user.name} avatar's`} src={user.avatar}/>}
+        {!user ? 'Loading user...' : <img alt={`${user.firstName}'s avatar`} src={user.avatar}/>}
       </div>
       {message.message}
-      <span>{moment().format('2016-03-08T20:11:54Z')}</span>
+      <span>{moment().format(message.timestamp)}</span>
     </li>
   )
 }
