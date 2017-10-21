144f6e345f060c22bb74b1f441d6698e4bfeec0f
Jordi Mundo <jordi.mundo@ZHQ-00383.local>
2017-10-21 14:27:44 +0100

    -Show and hide email on hover

--- src/MessagesListItem.jsx
+++ src/MessagesListItem.jsx
@@ -4,12 +4,17 @@ import moment from 'moment';
 export default ({users, message}) => {
   const user = users.isPending ? false : users.users.find(i=> i.id === message.userId);
   return (
-    <li>
-      <div>
+    <li className="message-item">
+      <div className="avatar">
         {!user ? 'Loading user...' : <img alt={`${user.firstName}'s avatar`} src={user.avatar}/>}
       </div>
-      {message.message}
-      <span>{moment().format(message.timestamp)}</span>
+      <div className="message">
+        {message.message}
+        <span>{moment().format(message.timestamp)}</span>
+      </div>
+      <div className="email">
+        {!user ? 'Loading user...' : <p>{user.email}</p>}
+      </div>
     </li>
   )
 }
